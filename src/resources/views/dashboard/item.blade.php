c65c5ceca506b50026b5ee0839402b5291ea6cb9
Gareth Midwood <gareth.midwood@creode.co.uk>
2018-06-14 15:02:51 +0100

    Remove commented out dashboard tile code

--- src/resources/views/dashboard/item.blade.php
+++ src/resources/views/dashboard/item.blade.php
@@ -5,35 +5,3 @@ items.push({
     "test_url": "{{ $project->test_url }}"
 });
 
-
-
-/**
-<table class="project-dashboard-table">
-    <tr class="heading">
-        <td colspan="3">
-            <div class="title">
-                {{ $project->project_name }}
-            </div>
-            <div class="buttons">
-                <a class="showhide" href="#" onclick="$(this).parent().parent().parent().nextUntil( 'tr.heading' ).toggle(); $(this).parent().siblings('.summary').toggle(); $(this).parent().siblings('.title').toggle(); return false;">Min/Maximise</a>
-                <a class="delete" href="/project/delete/{{ $project->id }}" onclick="return confirm('Are you sure you want to delete this project? This cannot be undone');">Delete Project</a>
-            </div>
-            <div class="summary" style="display: none">
-                <table>
-                    <tr>
-                        <td style="width: 40%"><strong>{{ $project->project_name }}</strong></td>
-                        <td style="width: 10%; text-align: right"><strong>Live Status:&nbsp;</strong></td>
-                        <td class="status" style="width: 10%;">OK</td>
-                        <td style="width: 10%; text-align: right"><strong>Test Status:&nbsp;</strong></td>
-                        <td class="status-test" style="width: 10%;">OK</td>
-                        <td style="width: 20%"></td>
-                    </tr>
-                </table>
-            </div>
-        </td>
-    </tr>
-
-    @include('dashboard.environment', array('project' => $project, 'environment' => 'live', 'tool_url' => $project->live_url))
-    @include('dashboard.environment', array('project' => $project, 'environment' => 'test', 'tool_url' => $project->test_url))
-</table>
-**/
